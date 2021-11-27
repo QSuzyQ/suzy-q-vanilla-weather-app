50831979253dcad38c8105b2ae85d715016f4b7b
QSuzyQ <92366704+QSuzyQ@users.noreply.github.com>
2021-11-27 23:46:18 +0100

    Updated the date and time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,46 @@
+function formatDate(actualDate) {
+  let hours = actualDate.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = actualDate.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayIndex = actualDate.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+  let monthIndex = actualDate.getMonth();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[monthIndex];
+  let date = actualDate.getDate();
+  let year = actualDate.getFullYear();
+  let currentTime = `${hours}:${minutes}`;
+  let currentDate = `${day}, ${month} ${date}, ${year}`;
+  return [currentDate, currentTime];
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -12,6 +55,12 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
 }
+
+let dateElement = document.querySelector("#date");
+let timeElement = document.querySelector("#time");
+let actualDate = new Date();
+dateElement.innerHTML = formatDate(actualDate)[0];
+timeElement.innerHTML = formatDate(actualDate)[1];
 
 let apiKey = `066545da4c8046912b00adb2744905ad`;
 let city = `Salzburg`;
