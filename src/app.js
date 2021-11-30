51228f2bdb4990ba51792eb6e9cf2361f2a257ea
QSuzyQ <92366704+QSuzyQ@users.noreply.github.com>
2021-11-30 22:54:43 +0100

    Added dummy forecast HTML through JS

--- src/app.js
+++ src/app.js
@@ -41,6 +41,34 @@ function formatDate(actualDate) {
   return [currentDate, currentTime];
 }
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Mon"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+                  <div class="col-2">
+                    <div class="weather-forecast-date">${day}</div>
+                    <img
+                      src="http://openweathermap.org/img/wn/13d@2x.png"
+                      alt=""
+                      width="36"
+                    />
+                    <div class="weather-forecast-temperatures">
+                      <span class="weather-forecast-temperature-max">18° </span>
+                      <span class="weather-forecast-temperature-min">12°</span>
+                    </div>
+                  </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -122,3 +150,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Zell am See");
+displayForecast();
