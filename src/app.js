a593950392723c5610c5cf78369202259d484f4c
QSuzyQ <92366704+QSuzyQ@users.noreply.github.com>
2021-11-30 23:40:37 +0100

    Added the API Url and got the forecast data

--- src/app.js
+++ src/app.js
@@ -41,7 +41,8 @@ function formatDate(actualDate) {
   return [currentDate, currentTime];
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "Mon"];
@@ -69,6 +70,14 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = `066545da4c8046912b00adb2744905ad`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -93,6 +102,9 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  console.log(response.data);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -150,4 +162,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Zell am See");
-displayForecast();
